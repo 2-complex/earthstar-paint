550c9bc0917e124d292ff324e4810192c36ec2db
Peterson Trethewey <peterson@2-complex.com>
2020-11-26 11:24:09 -0800

    Reduced dependence on ids for finding objects in the editor

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,45 +1,74 @@
 
 function make_picture_frame(width, height)
 {
-    return $("<div>", {id:"picture-frame-1", "class":"picture-frame"}).append(
-        $("<canvas>", {id:"painting", "class":"painting"})
-            .attr({"width":width,"height":height}),
-        $("<canvas>", {id:"painting-overlay", "class":"painting overlay"})
-            .attr({"width":width,"height":height})
-    ).css({"width":width, "height":height});
+    let picture_frame = $("<div>", {id:"picture-frame-1", "class":"picture-frame"});
+    picture_frame.css({"width":width, "height":height});
+
+    let painting = $("<canvas>", {id:"painting", "class":"painting"})
+        .attr({"width":width,"height":height});
+    let overlay = $("<canvas>", {id:"painting-overlay", "class":"painting overlay"})
+        .attr({"width":width,"height":height});
+
+    picture_frame.append(painting, overlay);
+
+    init_paint_brush_events(picture_frame, painting, overlay);
+    return picture_frame;
 }
 
 function make_tools_frame()
 {
-    return $("<div>", {id:"tools-frame-1", "class":"tools-frame"}).append(
+    let $hue = $("<div>", {id:"hue", "class": "slider-container"});
+    let $saturation = $("<div>", {id:"saturation", "class": "slider-container"});
+    let $lightness = $("<div>", {id:"lightness", "class": "slider-container"});
+    let $radius = $("<div>", {id:"radius", "class": "slider-container"});
+    let $info_display_hsv = $("<div>", {id:"info-display-hsv", "class":"info-display"});
+    let $info_display_radius = $("<div>", {id:"info-display-radius", "class":"info-display"});
+    let $swatch = $("<canvas>", {"class":"swatch", id:"swatch"}).attr({'width':100,'height':100});
+
+    let tools_frame = $("<div>", {id:"tools-frame-1", "class":"tools-frame"}).append(
         $("<div>", {id:"hue-labeled-slider", "class":"labeled-slider"}).append(
-            $("<div>", {"class": "prop-label"}).text("L"),
-            $("<div>", {id:"hue", "class": "slider-container"})
+            $("<div>", {"class": "prop-label"}).text("H"),
+            $hue
         ),
         $("<div>", {id:"saturation-labeled-slider", "class":"labeled-slider"}).append(
-            $("<div>", {"class": "prop-label"}).text("L"),
-            $("<div>", {id:"saturation", "class": "slider-container"})
+            $("<div>", {"class": "prop-label"}).text("S"),
+            $saturation
         ),
         $("<div>", {id:"lightness-labeled-slider", "class":"labeled-slider"}).append(
             $("<div>", {"class": "prop-label"}).text("L"),
-            $("<div>", {id:"lightness", "class": "slider-container"})
+            $lightness
         ),
         $("<div>", {id:"radius-labeled-slider", "class":"labeled-slider"}).append(
             $("<div>", {"class": "prop-label"}).text("R"),
-            $("<div>", {id:"radius", "class": "slider-container"})
+            $radius
         ),
-        $("<canvas>", {"class":"swatch", id:"swatch"}).attr({'width':100,'height':100}),
-        $("<div>", {id:"info-display-hsv", "class":"info-display"}),
-        $("<div>", {id:"info-display-radius", "class":"info-display"})
-    )
+        $swatch,
+        
+        $info_display_hsv,
+        $info_display_radius
+    );
+
+    init_paint_tools({
+         $hue : $hue,
+         $saturation : $saturation,
+         $lightness : $lightness,
+         $radius : $radius,
+         $info_display_hsv : $info_display_hsv,
+         $info_display_radius : $info_display_radius,
+         $swatch : $swatch,
+    });
+
+    return tools_frame;
 }
 
-function make_editor()
+function make_editor(width, height)
 {
-    return $("<div>", {id:"editor"}).append(
-        make_picture_frame(800, 540),
+    let editor = $("<div>", {id:"editor"}).append(
+        make_picture_frame(width, height),
         make_tools_frame()
     );
+
+    return editor;
 }
 
 var brush_settings = {
@@ -117,18 +146,15 @@ function get_point_in_element(evt, elem)
     return {x: x, y: y};
 }
 
-function init_paint_brush_events()
+function init_paint_brush_events(frame, painting, overlay)
 {
-    let frame = $("#picture-frame-1");
     let dragging = false;
     let last_x = 0;
     let last_y = 0;
 
-    let painting = $("#painting");
     let painting_canvas = painting[0];
     let painting_ctx = painting_canvas.getContext('2d');
 
-    let overlay = $("#painting-overlay");
     let overlay_canvas = overlay[0];
     let overlay_ctx = overlay_canvas.getContext('2d');
 
@@ -212,28 +238,28 @@ function lightness_background_color_sequence(h, s)
     return seq.join(", ")
 }
 
-function init_paint_tools()
+function init_paint_tools(tool_info)
 {
     function refresh_swatch()
     {
-        var h = $( "#hue" ).slider( "value" );
-        var s = $( "#saturation" ).slider( "value" );
-        var l = $( "#lightness" ).slider( "value" );
-        var radius = $( "#radius" ).slider( "value" );
+        var h = tool_info.$hue.slider( "value" );
+        var s = tool_info.$saturation.slider( "value" );
+        var l = tool_info.$lightness.slider( "value" );
+        var radius = tool_info.$radius.slider( "value" );
 
         brush_settings.h = h;
         brush_settings.s = s;
         brush_settings.l = l;
         brush_settings.radius = radius;
 
-        $("#hue").css({"background-image": "linear-gradient(to right, " + hue_background_color_sequence(s, l) + ")"});
-        $("#saturation").css({"background-image": "linear-gradient(to right, " + saturation_background_color_sequence(h, l) + ")"});
-        $("#lightness").css({"background-image": "linear-gradient(to right, " + lightness_background_color_sequence(h, s) + ")"});
+        tool_info.$hue.css({"background-image": "linear-gradient(to right, " + hue_background_color_sequence(s, l) + ")"});
+        tool_info.$saturation.css({"background-image": "linear-gradient(to right, " + saturation_background_color_sequence(h, l) + ")"});
+        tool_info.$lightness.css({"background-image": "linear-gradient(to right, " + lightness_background_color_sequence(h, s) + ")"});
 
-        $('#info-display-hsv').text(h + " " + s + "% " + l + "%");
-        $('#info-display-radius').text(radius);
+        tool_info.$info_display_hsv.text(h + " " + s + "% " + l + "%");
+        tool_info.$info_display_radius.text(radius);
 
-        let swatch_canvas = $("#swatch")[0];
+        let swatch_canvas = tool_info.$swatch[0];
         let swatch_ctx = swatch_canvas.getContext('2d');
         swatch_ctx.clearRect(0, 0, swatch_canvas.width, swatch_canvas.height);
             draw_stroke_segment(swatch_ctx, 0, 100, 50, 50, radius, hsl_string(h, s, l));
@@ -252,17 +278,18 @@ function init_paint_tools()
         };
     }
 
-    $( "#saturation, #lightness" ).slider(slider_config(100));
-    $( "#hue" ).slider(slider_config(360));
-    $( "#radius" ).slider(slider_config(200));
+    tool_info.$hue.slider(slider_config(360));
+    tool_info.$saturation.slider(slider_config(100));
+    tool_info.$lightness.slider(slider_config(100));
+    tool_info.$radius.slider(slider_config(200));
 
     let h = brush_settings.h;
     let s = brush_settings.s;
     let l = brush_settings.l;
     let radius = brush_settings.radius;
 
-    $( "#hue" ).slider( "value", h );
-    $( "#saturation" ).slider( "value", s );
-    $( "#lightness" ).slider( "value", l );
-    $( "#radius" ).slider( "value", radius );
+    tool_info.$hue.slider( "value", h );
+    tool_info.$saturation.slider( "value", s );
+    tool_info.$lightness.slider( "value", l );
+    tool_info.$radius.slider( "value", radius );
 }
--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,6 @@
 $(document).ready(
     function()
     {
-        $("body").append(make_editor());
-        init_paint_brush_events();
-        init_paint_tools();
+        $("body").append(make_editor(800, 540));
     }
 );
